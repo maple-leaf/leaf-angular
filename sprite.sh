ae10e68db739c3f0933fab9f848886c2eb4ba3cb
maple-leaf <tangjf.88@163.com>
2015-05-13 16:35:21 +0800

    Init gh-pages

--- sprite.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-css-sprite src/scss/images src/scss/images/sprites/*.png --style=src/scss/partials/sprite.scss --processor=scss --name=icons --css-image-path=images --template=sprite-scss-template.mustache
