470373e271c54718b09299d4a06b3c0331dfdac2
maple-leaf <tangjf.88@163.com>
2015-06-05 15:38:28 +0800

    Fix calculation of exceedHeight of leaf-content when leaf-content init: calculat in

--- www/js/leaf.js
+++ www/js/leaf.js
@@ -2537,7 +2537,6 @@ window.WebKitCSSMatrix?i=new window.WebKitCSSMatrix("none"===s.webkitTransform?"
                         pullLoadOptions = JSON.parse(attrs.pullLoad);
                     }
                     loadingIcon = '<svg version="1.1" class="leaf-loading-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve"> <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/> <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z"> <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="1s" repeatCount="indefinite"/> </path> </svg>';
-                    exceedHeight = ele.children()[0].clientHeight - ele[0].clientHeight;
                     loadMoreHtml = document.createElement('div');
                     loadMoreHtml.innerHTML = loadingIcon + "<span class='fa fa-arrow-up'></span><span class='pull-load-text'>" + (pullLoadOptions.pullText || "Pull up to load more") + "</span><span class='release-load-text'>" + (pullLoadOptions.releaseText || "Release to load more") + "</span>";
                     loadMoreHtml.setAttribute('class', 'leaf-load-more');
@@ -2566,6 +2565,9 @@ window.WebKitCSSMatrix?i=new window.WebKitCSSMatrix("none"===s.webkitTransform?"
                                 });
                             });
                         }
+                    });
+                    $timeout(function() {
+                        exceedHeight = ele.children()[0].clientHeight - ele[0].clientHeight;
                     });
                 }
                 $rootScope.$refresh = scope.$refresh = function() {
