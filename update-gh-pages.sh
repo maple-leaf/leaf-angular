74f3612a066916b978962a3f56a1e50493dc53d7
maple-leaf <tangjf.88@163.com>
2015-07-21 15:00:39 +0800

    fix update-gh-pages.sh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,4 +3,7 @@ cp -rf www ../leaf-gh-pages-www
 git checkout gh-pages
 cp -rf ../leaf-gh-pages-www/* .
 rm -rf ../leaf-gh-pages-www
-#git checkout master
+git add -A
+git commit -m "update gh-pages"
+git push
+git checkout master
