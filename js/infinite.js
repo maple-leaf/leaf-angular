8c16de53bc24947a9938f27fe4aca530f1785b11
maple-leaf <tangjf.88@163.com>
2015-08-10 14:31:00 +0800

    Add infinite demo and some modifies

---
+++ js/infinite.js
@@ -0,0 +1,9 @@
+leafDemo.controller('infiniteCtrl', function($scope, $http) {
+	$scope. datas = ["data","data","data","data","data","data","data","data","data","data","data","data","data"];
+	$scope.loadMore = function(cb) {
+		$http.get('/data.json').success(function(data) {
+			$scope.datas = $scope.datas.concat(data);
+			cb(); // This must be called after everything done
+		});
+	};
+});
