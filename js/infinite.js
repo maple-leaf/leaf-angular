115cdfaad6733500f2aab3b9f3c5faf90bfb85dd
maple-leaf <tangjf.88@163.com>
2015-08-10 14:39:02 +0800

    move data.json to folder demo

--- js/infinite.js
+++ js/infinite.js
@@ -1,7 +1,7 @@
 leafDemo.controller('infiniteCtrl', function($scope, $http) {
 	$scope. datas = ["data","data","data","data","data","data","data","data","data","data","data","data","data"];
 	$scope.loadMore = function(cb) {
-		$http.get('/data.json').success(function(data) {
+		$http.get('data.json').success(function(data) {
 			$scope.datas = $scope.datas.concat(data);
 			cb(); // This must be called after everything done
 		});
